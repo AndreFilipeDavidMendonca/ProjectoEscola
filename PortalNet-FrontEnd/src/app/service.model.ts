30dba35694e01b7f4a922612cdb95122d6e127e9
André Mendonça <andredmendonca89@gmail.com>
2019-06-27 18:29:15 +0100

    quinta-feira

--- PortalNet-FrontEnd/src/app/service.model.ts
+++ PortalNet-FrontEnd/src/app/service.model.ts
@@ -4,18 +4,22 @@ export class Service {
     public internet: string;
     public tv: string;
     public mobilePhone: string;
-    public price: number;
+    public phone: string;
+    public price: string;
+    public loyalty: string;
     public creationDate: string;
     public status: boolean;
     
-    constructor (id: number, name: string, internet: string, tv: string, mobilePhone: string, price: number, creationDate: string, status: boolean)  {
+    constructor (id: number, name: string, internet: string, tv: string, mobilePhone: string, phone: string, price: string, loyalty: string, creationDate: string, status: boolean)  {
 
         this.id = id;
         this.name = name;
         this.internet = internet;
         this.tv = tv;
         this.mobilePhone = mobilePhone;
+        this.phone = phone;
         this.price = price;
+        this.loyalty = loyalty;
         this.creationDate = creationDate;
         this.status = status;
 
--- PortalNet-FrontEnd/src/app/services/services-table/services-table.component.ts
+++ PortalNet-FrontEnd/src/app/services/services-table/services-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Service } from 'src/app/service.model';
 
 @Component({
   selector: 'app-services-table',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-table.component.css']
 })
 export class ServicesTableComponent implements OnInit {
+  services: Service[] = [
+    new Service(1, 'Pacote 4 Serviços', '100 MB/s', '100 canais', '3GB', 'Grátis para redes fixas', '50€', '3 anos', '27/06/2019', true)
+  ];
 
-  constructor() { }
+  
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
