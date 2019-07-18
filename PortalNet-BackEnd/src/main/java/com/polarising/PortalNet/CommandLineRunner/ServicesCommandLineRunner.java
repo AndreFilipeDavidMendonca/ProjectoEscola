b92dd1a98ca8b6ffa6d8161c356081a0798eb220
André Mendonça <andredmendonca89@gmail.com>
2019-07-18 17:28:04 +0100

    Update Serviços, control de dados e tranformação de outputs!

--- PortalNet-BackEnd/src/main/java/com/polarising/PortalNet/CommandLineRunner/ServicesCommandLineRunner.java
+++ PortalNet-BackEnd/src/main/java/com/polarising/PortalNet/CommandLineRunner/ServicesCommandLineRunner.java
@@ -15,16 +15,16 @@ public class ServicesCommandLineRunner implements CommandLineRunner {
 	
 	public void run(String... args) throws Exception {
 		
-		serviceRepository.save(new Services("Blue Master 1", "100", "200", "Minutos Ilimitados",
-				"Minutos Ilimitados", 1, 100, "01-01-2019", true, "assets/img/servicesBackground0.png", "servicesBackground0.png"));
-		serviceRepository.save(new Services("Blue Master 2", "50", "100", "Minutos Ilimitados",
-				"Minutos Ilimitados", 1, 60, "01-01-2019", true, "assets/img/servicesBackground1.png", "servicesBackground1.png"));
-		serviceRepository.save(new Services("Blue Master 3", "Sem Internet", "200", "Minutos Ilimitados",
-				"Minutos Ilimitados", 1, 45, "01-01-2019", true, "assets/img/servicesBackground2.png", "servicesBackground2.png"));
-		serviceRepository.save(new Services("Blue Master 4", "200", "0", "Minutos Ilimitados",
-				"Minutos Ilimitados", 1, 30, "01-01-2019", true, "assets/img/servicesBackground3.png", "servicesBackground3.png"));
+		serviceRepository.save(new Services("Blue Master 1", "100", "200", "1500",
+				"15", 5, 100, "01-01-2019", true, "assets/img/servicesBackground0.png", "servicesBackground0.png"));
+		serviceRepository.save(new Services("Blue Master 2", "50", "100", "2000",
+				"3", 1, 60, "01-01-2019", true, "assets/img/servicesBackground1.png", "servicesBackground1.png"));
+		serviceRepository.save(new Services("Blue Master 3", "", "200", "",
+				"5", 8, 45, "01-01-2019", true, "assets/img/servicesBackground2.png", "servicesBackground2.png"));
+		serviceRepository.save(new Services("Blue Master 4", "200", "", "5000",
+				"", 0, 30, "01-01-2019", true, "assets/img/servicesBackground3.png", "servicesBackground3.png"));
 		serviceRepository.save(new Services("Blue Master 5", "200", "10", "Minutos Ilimitados",
-				"Minutos Ilimitados", 1, 15, "01-01-2019", true, "assets/img/servicesBackground4.png", "servicesBackground4.png"));
+				"20", 2, 15, "01-01-2019", true, "assets/img/servicesBackground4.png", "servicesBackground4.png"));
 		
 	}
 	
